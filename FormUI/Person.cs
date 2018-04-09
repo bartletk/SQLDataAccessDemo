f0d13ff0a9150107736bebeeac6d6f7f1d60d246
bartletk <bartletk@warhawks.ulm.edu>
2018-04-09 04:26:10 -0500

    app.config established

--- FormUI/Person.cs
+++ FormUI/Person.cs
@@ -12,6 +12,6 @@ namespace FormUI
         public  string FirstName{ get; set; }
         public string LastName { get; set; }
         public string EmailAddress { get; set; }
-        public string Phone  { get; set; }
+        public string PhoneNumber  { get; set; }
     }
 }
